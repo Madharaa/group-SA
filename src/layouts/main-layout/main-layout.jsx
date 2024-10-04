913ab08c68ddcf2f620b5fd76d75924144c2c023
WKCsachintha <wkcsachintha@students.nsbm.ac.lk>
2024-10-05 03:35:11 +0530

    add rowting path

---
+++ src/layouts/main-layout/main-layout.jsx
@@ -0,0 +1,30 @@
+// // import css
+// // import libraries
+// import { Layout } from "antd";
+// import { Outlet } from "react-router-dom";
+// // import components
+// // import SampleNav from "../../components/custom/sample-nav/sample-nav";
+// import Header from "../../pages/home/Header";
+// import Footer from "../../pages/home/Footer";
+// // Variable declarations
+// const { Content } = Layout;
+
+// // Component definition
+// export default function MainLayout() {
+//     // State variables
+//     // Hooks
+//     // Event handlers
+//     // Helper functions
+//     // Return statement
+//     return(
+//         <>
+//             <Layout>
+//                 <Header/>
+//                 <Content>
+//                     <Outlet />
+//                 </Content>
+//                 <Footer/>
+//             </Layout>
+//         </>
+//     )
+// }
